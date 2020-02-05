476e83ba534e06bfbab90e7c31bfcac59778b5cd
hetjoshi87 <hetjoshi87@gmail.com>
2020-02-04 23:14:40 -0600

    Checking in code

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObjectcs> CelestialObjectcs { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
---
+++ StarChart/Models/CelestialObjectcs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace StarChart.Models
+{
+    public class CelestialObjectcs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrbitedObject { get; set; }
+        [NotMapped]
+        public List<CelestialObjectcs> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }   
+}
