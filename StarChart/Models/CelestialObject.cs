e8b4d1c4bade1858d29020c1297f65eebdc1f2b9
hetjoshi87 <hetjoshi87@gmail.com>
2020-02-04 23:30:52 -0600

    Checking in properties

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -10,8 +10,9 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public int? OrbitedObject { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
